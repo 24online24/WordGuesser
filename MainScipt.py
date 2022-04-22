b096a51600b3bbff49f60ca38b7b94ba006ab94c
24online24 <chiuzbaianrares20@gmail.com>
2022-04-22 21:42:34 +0300

    Changed: input checking function. Added: upper and lower case matching, option to restart game.

--- MainScipt.py
+++ MainScipt.py
@@ -23,24 +23,29 @@ def get_input(message, type):
         print(message, end='')
         raw = input()
 
-        if raw == '' and type in strings:
-            return raw
+        if type == int:
+            if raw.isnumeric():
+                return int(raw)
 
-        elif type in strings and raw.isalpha():
-            if type == 'single_char':
-                if len(raw) < 2:
-                    return raw
-            else:
+        if type in strings:
+            if raw == '':
                 return raw
 
-        elif type == int and raw.isnumeric():
-            return int(raw)
+            if raw.isalpha():
+                raw = raw.lower()
+                if type == 'single_char':
+                    if len(raw) < 2:
+                        return raw
+                else:
+                    return raw
+
         print('Invalid input')
 
 
 def positional_check(set_to_check, positions_list):
     for element in set_to_check:
-        position = get_input(f"What position did you check {element} on? ", int)
+        position = get_input(
+            f"What position did you check \"{element}\" on? ", int)
         positions_list[position-1].append(element)
 
 
@@ -52,82 +57,101 @@ def add_input_to_set(main_set, message=''):
     main_set.update(aux_set)
 
 
-# depending on the specific game, words can have various lengths
-length = get_input('Word length: ', int)
-# a list of all words is initially created
-possibilities = create_word_list(length)
+def continue_check():
+    while True:
+        check = input("Restart? [Yes/ No] ")
+        if check in ('Yes', 'Y', 'yes', 'y', '1'):
+            return True
+        if check in ('No', 'N', 'no', 'n', '0'):
+            return False
 
-letter = list()  # the array of known letters
-for i in range(length):
-    letter.append('')
 
-contained = set()  # letters known to be in the word, but not the position
-not_contained = set()  # letters known to not be in the word
-# letters tried for every position, as lists in a tuple
-tried = tuple(([] for i in range(length)))
+def main():
+    # depending on the specific game, words can have various lengths
+    length = get_input('Word length: ', int)
+    # a list of all words is initially created
+    possibilities = create_word_list(length)
 
-while True:  # this will work for any number of rounds
-    print('Known letters (shown in green)')
-    for i in range(length):  # checks for new letters
-        if letter[i] == '':  # if a letter is known, its position is ignored for later checks
-            letter[i] = get_input(f'Letter {i+1}: ', 'single_char')
-            if letter[i] in contained:
-                contained.remove(letter[i])
+    letter = list()  # the array of known letters
+    for i in range(length):
+        letter.append('')
 
-    add_input_to_set(contained, "Contained (shown in yellow): ")
-    positional_check(contained, tried)
-    add_input_to_set(not_contained, "Not contained (shown in black): ")
+    contained = set()  # letters known to be in the word, but not the position
+    not_contained = set()  # letters known to not be in the word
+    # letters tried for every position, as lists in a tuple
+    tried = tuple(([] for i in range(length)))
 
-    possibilitiesAUX = list()
-    for word in possibilities:  # checks every word from the current possibilities
+    while True:  # this will work for any number of rounds
+        print('Known letters (shown in green)')
+        for i in range(length):  # checks for new letters
+            if letter[i] == '':  # if a letter is known, its position is ignored for later checks
+                letter[i] = get_input(f'Letter {i+1}: ', 'single_char')
+                if letter[i] in contained:
+                    contained.remove(letter[i])
 
-        ok_known = True  # the word supposedly matches all known letters
-        for i in range(length):  # checks for every letter
-            if letter[i]:  # if that position has a known letter
-                if word[i] != letter[i]:  # checks if the word matches
-                    ok_known = False  # if one letter doesn't match, the word is not valid
+        add_input_to_set(contained, "Contained (shown in yellow): ")
+        positional_check(contained, tried)
+        add_input_to_set(not_contained, "Not contained (shown in black): ")
 
-        ok_contained = True  # checks if all contained letters appear in word
-        for character in contained:  # every letter that is contained must be checked
-            ok_contained = False  # supposes a letter does not appear
-            for i in range(length):  # checks for every letter position
-                if letter[i] == '':  # ignores positions with known letters in them
-                    if character == word[i]:  # if the letter appears
-                        # and if it has not been tried in this position already
-                        if character not in tried[i]:
-                            ok_contained = True  # the boolean variable is switched
-                        break  # it doesn't check for said letter anymore
-            if ok_contained == False:  # it a letter didn't appear
-                break  # it doesn't check for other letters
+        possibilitiesAUX = list()
+        for word in possibilities:  # checks every word from the current possibilities
 
-        # supposes the word is valid and doesn't containe any character it shouldn't
-        ok_not_contained = True
-        for character in not_contained:  # ever letter that we know is not contained must not be present
-            if character not in contained:
+            ok_known = True  # the word supposedly matches all known letters
+            for i in range(length):  # checks for every letter
+                if letter[i]:  # if that position has a known letter
+                    if word[i] != letter[i]:  # checks if the word matches
+                        ok_known = False  # if one letter doesn't match, the word is not valid
+
+            ok_contained = True  # checks if all contained letters appear in word
+            for character in contained:  # every letter that is contained must be checked
+                ok_contained = False  # supposes a letter does not appear
                 for i in range(length):  # checks for every letter position
                     if letter[i] == '':  # ignores positions with known letters in them
-                        if character == word[i]:  # if one letter is present,
-                            ok_not_contained = False  # the word is not valid
+                        if character == word[i]:  # if the letter appears
+                            # and if it has not been tried in this position already
+                            if character not in tried[i]:
+                                ok_contained = True  # the boolean variable is switched
+                            break  # it doesn't check for said letter anymore
+                if ok_contained == False:  # it a letter didn't appear
+                    break  # it doesn't check for other letters
 
-        if ok_known and ok_contained and ok_not_contained:  # if yes, it is added for the next round of checking
-            possibilitiesAUX.append(word)
+            # supposes the word is valid and doesn't containe any character it shouldn't
+            ok_not_contained = True
+            for character in not_contained:  # ever letter that we know is not contained must not be present
+                if character not in contained:
+                    for i in range(length):  # checks for every letter position
+                        if letter[i] == '':  # ignores positions with known letters in them
+                            if character == word[i]:  # if one letter is present,
+                                ok_not_contained = False  # the word is not valid
 
-    possibilities = possibilitiesAUX  # the list of possible words is updated
-    print('------------------------------------')
-    print('Possible words:')
-    print(possibilities)  # shows the remaining words
-    print('Letters: ', end='')  # prints the known word format
-    for i in range(length):
-        if letter[i]:
-            print(letter[i], end='')
-        else:  # the unknown letters are substituted by an underscore
-            print('_', end='')
-    print()
-    print('Contained:', ', '.join(character for character in contained))
-    print('Tried: ')
-    for i in range(length):
-        print(f' {i+1}: ', end='')
-        print(tried[i])
+            if ok_known and ok_contained and ok_not_contained:  # if yes, it is added for the next round of checking
+                possibilitiesAUX.append(word)
 
-    print('Not contained:', ', '.join(character for character in not_contained))
-    print('------------------------------------')
+        possibilities = possibilitiesAUX  # the list of possible words is updated
+        print('------------------------------------')
+        print('Possible words:')
+        print(possibilities)  # shows the remaining words
+        print('Letters: ', end='')  # prints the known word format
+        for i in range(length):
+            if letter[i]:
+                print(letter[i], end='')
+            else:  # the unknown letters are substituted by an underscore
+                print('_', end='')
+        print()
+        print('Contained:', ', '.join(character for character in contained))
+        print('Tried: ')
+        for i in range(length):
+            print(f' {i+1}: ', end='')
+            print(tried[i])
+
+        print('Not contained:', ', '.join(
+            character for character in not_contained))
+        print('------------------------------------')
+        restart = continue_check()
+        if restart:
+            break
+
+
+if __name__ == '__main__':
+    while True:
+        main()
